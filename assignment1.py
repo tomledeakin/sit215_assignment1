f65803884d525bbccd42b354aa42a0d6a8408edb
tomledeakin <tomledeakin@gmai.com>
2025-03-26 02:21:02 +1100

    update

--- assignment1.py
+++ assignment1.py
@@ -19,7 +19,7 @@ importlib.reload(problem)
 
 from problem import Problem, Node, GraphProblem, Graph, UndirectedGraph
 
-get_ipython().run_line_magic('matplotlib', 'inline')
+# get_ipython().run_line_magic('matplotlib', 'inline')
 
 
 # In[2]:
@@ -753,10 +753,10 @@ for res in results:
 # run_gui()
 
 
-# In[7]:
+# In[8]:
 
 
-get_ipython().system('jupyter nbconvert --to script your_notebook.ipynb')
+get_ipython().system('jupyter nbconvert --to script assignment1.ipynb')
 
 
 # In[ ]:
